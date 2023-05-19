0e8aa162256c804d8ae78c9a30ab6a84afb5e74c
HamishDrogemuller <79230862+HamishDrogemuller@users.noreply.github.com>
2023-05-19 13:42:36 +1200

    Minor update to product

--- MESN/backend/data/productData.js
+++ MESN/backend/data/productData.js
@@ -2,7 +2,7 @@ const products = [
   {
     "name": "Administrator Training",
     "imageUrl":
-      "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
+      "",
     "description":
       "A basic training course for Administration",
     "voucherPrice": 1,
@@ -12,7 +12,7 @@ const products = [
   {
     "name": "Car Basics",
     "imageUrl":
-      "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
+      "",
     "description":
       "A basic course that provides the necessary training for managing a personal vehicle",
     "voucherPrice": 1,
@@ -22,7 +22,7 @@ const products = [
   {
     "name": "Public speaking skills",
     "imageUrl":
-      "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
+      "",
     "description":
       "Simple course that helps people find their voice",
     "voucherPrice": 1,
